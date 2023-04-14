092c47f2462696b7f01388950c92d2ece65b1c2f
syl-kailey <kailesyl000@gmail.com>
2023-04-14 13:15:35 -0400

    Bottom Fix

---
+++ Shoes.java
@@ -0,0 +1,14 @@
+public class Shoes extends Clothing{
+
+    boolean hasHeel;
+
+    public Shoes (String color, String pattern, String aesthetic, int outfit, boolean hasHeel){
+        super(color, pattern, aesthetic, outfit);
+        this.hasHeel = hasHeel;
+    }
+
+    public boolean hasHeel(){
+        return hasHeel;
+    }
+
+}
