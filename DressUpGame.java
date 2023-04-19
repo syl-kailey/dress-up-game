b78e00dc9aa41f558d61d896303226743c86b895
syl-kailey <kailesyl000@gmail.com>
2023-04-19 12:27:04 -0400

    set up User, Outfit, and almost DressUpGame

--- DressUpGame.java
+++ DressUpGame.java
@@ -1,3 +1,37 @@
+import java.util.*;
+import java.lang.*;
+
 public class DressUpGame {
-    
+    User user1;
+    User user2;
+    Outfit finalOutfit;
+    List<String> colors;
+    List<String> patterns;
+    List<String> aesthetics;
+    List<String> favoritePattern;
+
+    public String favoriteColor(User user1, User user2){
+        String hello = "hi";
+
+        return hello;
+    }
+
+    public String favoritePattern(User user1, User user2){
+        String hello = "hi";
+
+        return hello;
+    }
+
+    public String favoriteAesthetic(User user1, User user2){
+        String hello = "hi";
+
+        return hello;
+    }
+
+
+
+    public static void main(String[] args) {
+        
+    }
+
 }
