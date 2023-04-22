bb0c4a24c77d67b357132fb437d031f7bddab8a9
elise-hedlund <elisemariehedlund@gmail.com>
2023-04-22 16:47:00 -0400

    started building GUI

--- ShirtGUI.java
+++ ShirtGUI.java
@@ -1,3 +1,29 @@
-public class ShirtGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import javax.swing.plaf.metal.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+public class ShirtGUI extends JFrame {
+
+    JTextArea instructions; 
     
+    public ShirtGUI () {
+        instructions = new JTextArea ("Please select your three favorite shirts from the options below: "); 
+        instructions.setEditable(false); 
+
+        this.setSize(250,250);
+        this.add(instructions, BorderLayout.NORTH); 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
+
+    public static void main (String args[]) {
+        ShirtGUI test = new ShirtGUI(); 
+        test.setVisible(true);
+    }
+
 }
